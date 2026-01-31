50ee77910d307c2391573d870219ec3d81e8a393
Ben Landon <landb0x207@gmail.com>
2026-01-30 16:27:35 -0800

    feat: automatic history

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
+use std::time::Instant;
+
 use axum::{
     Router,
+    extract::State,
     http::header::{CONTENT_TYPE, HeaderMap, HeaderValue},
     routing::get,
 };
 use fake::Fake;
 
-async fn root() -> (HeaderMap, String) {
-    let now = chrono::Utc::now();
-    let lorem_title = {
-        let faker = fake::faker::lorem::en::Words(5..10);
-        faker.fake::<Vec<String>>().join(" ")
+const TIME_RATE: i64 = 60; // Once a minute
+
+#[derive(Clone)]
+struct AppState {
+    start: chrono::DateTime<chrono::Utc>,
+}
+
+async fn root(State(state): State<AppState>) -> (HeaderMap, String) {
+    let count = {
+        let time_delta = chrono::Utc::now() - state.start;
+        time_delta.num_seconds() / TIME_RATE
     };
-    let lorem_body = {
-        let faker = fake::faker::lorem::en::Paragraphs(5..10);
-        faker.fake::<Vec<String>>().join("\n")
+    let now = chrono::Utc::now();
+    let item_builder = |i| {
+        let lorem_title = {
+            let faker = fake::faker::lorem::en::Words(5..10);
+            faker.fake::<Vec<String>>().join(" ")
+        };
+        let lorem_body = {
+            let faker = fake::faker::lorem::en::Paragraphs(5..10);
+            faker.fake::<Vec<String>>().join("\n")
+        };
+        let item_time = now - chrono::Duration::seconds(TIME_RATE * i as i64);
+        rss::ItemBuilder::default()
+            .title(Some(lorem_title))
+            .content(Some(lorem_body))
+            .pub_date(Some(item_time.to_rfc2822()))
+            .build()
     };
     let channel = rss::ChannelBuilder::default()
         .title("Lorem Ipsum RSS")
-        .items(vec![
-            rss::ItemBuilder::default()
-                .title(Some(lorem_title))
-                .content(Some(lorem_body))
-                .pub_date(Some(now.to_rfc2822()))
-                .build(),
-        ])
+        .items((0..count).map(item_builder).collect::<Vec<rss::Item>>())
         .build();
 
     let string = channel.to_string();
@@ -35,11 +51,16 @@ async fn root() -> (HeaderMap, String) {
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let state = AppState {
+        start: chrono::Utc::now(),
+    };
+    let app = Router::new().route("/", get(root)).with_state(state);
     let port = std::env::args().nth(1).unwrap_or("3000".to_string());
 
     println!("Listening on port {port}");
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 }
